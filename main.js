e08bb5f3f83533575a39e4382f48efb81d7c6483
Athouba-Pebam <71960090+Athouba-Pebam@users.noreply.github.com>
2020-10-18 14:41:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+family_member_book
+
+function next1(){
+
+    document.getElementById("next_button").style.display="none";
+    document.getElementById("next_button1").style.display="inline-block";
+    
+    document.getElementById("family_img").style.display="none";
+    document.getElementById("papa_img").style.display="inline-block";
+
+    document.getElementById("family").style.display="none";
+    document.getElementById("papa").style.display="inline-block";
+    
+}
+
+
+
+
+function next2(){
+
+    document.getElementById("next_button1").style.display="none";
+    document.getElementById("next_button2").style.display="inline-block";
+    
+    document.getElementById("papa_img").style.display="none";
+    document.getElementById("mama_img").style.display="inline-block";
+
+    document.getElementById("papa").style.display="none";
+    document.getElementById("mama").style.display="inline-block";
+    
+}
+
+
+
+
+
+
+function next3(){
+
+    document.getElementById("next_button2").style.display="none";
+    document.getElementById("next_button3").style.display="inline-block";
+    
+    document.getElementById("mama_img").style.display="none";
+    document.getElementById("thoihenba_img").style.display="inline-block";
+
+    document.getElementById("mama").style.display="none";
+    document.getElementById("thoihenba").style.display="inline-block";
+    
+}
+
+
+
+
+
+
+function next4(){
+
+    
+    document.getElementById("next_button3").style.display="none"
+    document.getElementById("thoihenba_img").style.display="none";
+    document.getElementById("me_img").style.display="inline-block";
+
+    document.getElementById("thoihenba").style.display="none";
+    document.getElementById("athouba").style.display="inline-block";
+    
+}
